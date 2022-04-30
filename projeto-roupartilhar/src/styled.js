2bc0062abd6222e4d271a6c7ed9e5abdc1e0c0ff
Thamires Lippelt <thamivieira@gmail.com>
2022-04-30 19:23:33 -0300

    alterações

--- projeto-roupartilhar/src/styled.js
+++ projeto-roupartilhar/src/styled.js
@@ -13,19 +13,29 @@ export const SubTitle = styled.section`
     text-align: center;
     flex-direction: column;
     font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
-    color: var(--main-text-color);
+    color: #2d3830;
     font-size: 1.5rem;
     line-height: 1.2;
     font-weight: bolder;
 `
 
-export const InfosContainer = styled.section`
-  display: block;
-  background-color: #556B2F;
+export const MiddleContainer = styled.section`
+  background-color: #71ba5b;
   margin-top: 80px;
   height: 8vw;
-  font-family: 'Rubik', sans-serif;
+  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
   color: white;
   font-weight: 400;
   line-height: 1.5;
+  text-align: center;
+`
+export const ButtonContainer = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 40px;
+`
+export const InfosContainer = styled.section`
+  display: flex;
+  
 `
