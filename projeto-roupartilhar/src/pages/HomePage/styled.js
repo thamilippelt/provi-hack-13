1f70f498aa884bc7c97f5540964fcfcdcc8abfef
Thamires Lippelt <thamivieira@gmail.com>
2022-05-01 19:46:34 -0300

    arrumando layout

--- projeto-roupartilhar/src/pages/HomePage/styled.js
+++ projeto-roupartilhar/src/pages/HomePage/styled.js
@@ -4,6 +4,7 @@ export const ImgLogo = styled.img`
    display: block;
     margin-left: auto;
     margin-right: auto;
+    max-width: 100%;
 `
 export const SubTitle = styled.section`
     display: flex;
@@ -44,13 +45,19 @@ export const SubTitleText = styled.section`
   display: flex;
   margin-left: 60px;
   margin-top: 20px;
-  color: #2d3830;
+  color: #71ba5b;
   font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
  
 `
+export const ProductText = styled.section`
+  text-align: center;
+  color: #71ba5b;
+  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+  margin-bottom: 10px;
+`
 export const ImgSubText = styled.img`
-  max-width: 80vw;
-  max-height: 50vh;
+  max-width: 60vw;
+  max-height: 40vh;
 `
 export const ProductSection = styled.section`
     display: flex;
