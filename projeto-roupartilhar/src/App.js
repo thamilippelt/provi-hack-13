16cbdae9691c9bc1bf4cfbd4ac5f4420e7c5d28d
Thamires Lippelt <thamivieira@gmail.com>
2022-05-01 11:57:36 -0300

    tirei tudo do app.js e coloquei na homepage

--- projeto-roupartilhar/src/App.js
+++ projeto-roupartilhar/src/App.js
@@ -1,50 +1,15 @@
-import Button from '@mui/material/Button';
-import logo from './assets/logo.png'
-import { AppContainer, ButtonContainer, ImgLogo, InfosContainer, MiddleContainer, SubTitle } from './styled'
-import circular from './assets/economia-circular.png'
+import { AppContainer } from './styled'
 import Header from './components/Header/Header';
-import { useHistory } from 'react-router-dom';
-import { goToSignUp } from './router/coordinator';
+import Router from './router/Router';
 
 function App() {
-  const history = useHistory()
 
-  const buttonAction = () => {
-    goToSignUp(history)
-  }
 
   return (
     <AppContainer>
       <Header/>
-      <ImgLogo src={logo} />
-      <SubTitle>
-        <h1>ser elegante é ajudar o <u>mundo</u>. </h1>
-        <h6>CATALOGUE, COMPARTILHE, CUIDE DE SUAS ROUPAS E DO NOSSO PLANETA!</h6>
-      </SubTitle>
-      <ButtonContainer>
-        <Button variant="contained" color="success" onClick={() => buttonAction()} >Cadastre-se</Button>
-      </ButtonContainer>
-
-
-      <MiddleContainer>
-        <h3>procurando uma roupa para festa?</h3>
-        <h1>Você sabia que 50% - 80% das roupas de festa são usadas apenas UMA vez?</h1>
-      </MiddleContainer>
-      <InfosContainer>
-        <h6>O planeta é circular!
-          Por que nossas roupas não?
-        </h6>
-        <p>
-          Compartilhe, alugue, venda, troque (monetize sua roupas) ou mesmo contrate serviços de reparo, lavanderia, reciclagem,
-          doação e muito mais. Somos uma comunidade que disse basta! ao Fast Fashion e faz crescer uma economia colaborativa, da
-          troca entre pessoas e que ressignifica a sua roupa. Cadastre-se gratuitamente e participe.
-        </p>
-        <img src={circular}/>
-      </InfosContainer>
-      <>
-        Produtos
-      </>
-
+      <Router/>
+     
     </AppContainer>
 
 
--- projeto-roupartilhar/src/pages/Login/LoginPage.js
+++ projeto-roupartilhar/src/pages/Login/LoginPage.js
@@ -1,11 +1,76 @@
-const LoginPage = () => {
+import { useState } from "react";
 
+const LoginPage = () => {
+  const [errorMessages, setErrorMessages] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const database = [
+    {
+      username: "user1",
+      password: "pass1"
+    },
+    {
+      username: "user2",
+      password: "pass2"
+    }
+  ];
+
+  const errors = {
+    uname: "invalid username",
+    pass: "invalid password"
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    var { uname, pass } = document.forms[0];
+    const userData = database.find((user) => user.username === uname.value);
+
+    // Compara as informaçoes de user
+    if (userData) {
+      if (userData.password !== pass.value) {
+        // password inválido
+        setErrorMessages({ name: "pass", message: errors.pass });
+      } else {
+        setIsSubmitted(true);
+      }
+    }
+  };
+
+  // messagem de erro
+  const renderErrorMessage = (name) =>
+    name === errorMessages.name && (
+      <div>{errorMessages.message}</div>
+    );
+
+  // login form
+  const renderForm = (
+    <div className="form">
+      <form onSubmit={handleSubmit}>
+        <div className="input-container">
+          <label>Username </label>
+          <input type="text" name="uname" required />
+          {renderErrorMessage("uname")}
+        </div>
+        <div className="input-container">
+          <label>Password </label>
+          <input type="password" name="pass" required />
+          {renderErrorMessage("pass")}
+        </div>
+        <div className="button-container">
+          <input type="submit" />
+        </div>
+      </form>
+    </div>
+  );
     
 
     return (
-       <>
-       Login Page
-       </>
+        <div>
+        <div className="login-form">
+          <div className="title">Login</div>
+          {isSubmitted ? <div>Usuários logado com sucesso!</div> : renderForm}
+        </div>
+      </div>
     )
 }
 
